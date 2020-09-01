b860acd5aa06409a9ff7a22c8e893178a60a811b
Tom Sherman <tompsherman@gmail.com>
2020-09-01 16:27:13 -0400

    three events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+const button = document.querySelectorAll(".btn");
+const headlines = document.querySelectorAll("h2");
+const header = document.querySelector(".main-navigation");
+const navBar = document.querySelectorAll(".nav");
+const intro = document.querySelector(".intro p");
+
+header.addEventListener("dblclick", (event) => {
+  event.target.style.backgroundColor = "black";
+});
+header.addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "yellow";
+});
+header.addEventListener(
+  "click",
+  (event) => (event.target.style.fontSize = "30rem")
+);
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    header.style.backgroundColor = "white";
+  }
+});
