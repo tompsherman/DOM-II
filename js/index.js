d27ff27433428a75d90f6ae59c7e93c0bcc057ae
Tom Sherman <tompsherman@gmail.com>
2020-09-01 21:24:57 -0400

    10 events

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 // Your code goes here
 
 const button = document.querySelectorAll(".btn");
-const headlines = document.querySelectorAll("h2");
+const headline = document.querySelectorAll(".text-content ");
 const header = document.querySelector(".main-navigation");
 const navBar = document.querySelectorAll(".nav-link");
 const intro = document.querySelector(".intro p");
 const bus = document.querySelector(".intro img");
+const textContent = document.querySelector(".text-content");
 
 header.addEventListener("dblclick", (event) => {
   event.target.style.fontSize = "2rem";
@@ -49,3 +50,13 @@ bus.addEventListener("mouseup", (event) => {
 window.addEventListener("load", (event) => {
   intro.style.fontSize = "3rem";
 });
+
+// function textSelect(event) {
+//   const selection = event.target.value.substring(
+//     event.target.selectionStart,
+//     event.target.selectionEnd
+//   );
+//   textContent.textContent = `the new text is: ${selection}`;
+// }
+// const newH2 = document.querySelector(".text-content h2");
+// newH2.addEventListener("select", textSelect);
